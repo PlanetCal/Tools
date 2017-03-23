babd2c00d7777756f2c5a7654758ffa8bf8a33ce
AK <appireddy@hotmail.com>
2017-03-22 17:53:00 -0700

    Added Netwtonsoft.json and stub classes for parsing Tab, Comma separated files
    * Added Netwtonsoft.json Nuget package reference
    * Added stub classes for parsing Tab, Comma separated files

---
+++ ICSToPlanetCal/ParserModels/BaseCalendar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICSToPlanetCal.ParserModels
+{
+    internal class BaseCalendar
+    {
+        public string Source { get; set; }
+        public CalendarParameters Parameters { get; set; }
+
+        public string Location { get; set; }
+
+        public List<Event> Events { get; set; } = new List<Event>();
+
+        public BaseCalendar(string source)
+        {
+            this.Source = source;
+        }
+    }
+}
---
+++ ICSToPlanetCal/ParserModels/TabCommas/HeaderTabComma.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICSToPlanetCal.ParserModels.TabCommas
+{
+    internal class HeaderTabComma : BaseCalendar
+    {
+        public HeaderTabComma(string source) : base (source)
+        {
+            this.ProcessEvents();
+        }
+
+        private void ProcessEvents()
+        {
+        }
+    }
+}
